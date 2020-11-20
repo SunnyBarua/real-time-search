a37d13bb03ffee75a7a24c958a03cd333dbb80fe
SunnyBarua <sunnybarua95@gmail.com>
2020-11-20 20:40:44 +0600

    real time search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import algoliasearch from "algoliasearch/lite";
+import {
+  InstantSearch,
+  SearchBox,
+  Hits,
+  Pagination,
+  Stats,
+} from "react-instantsearch-dom";
+// KFWXVJ2EGD
+// 181bd9ae00288f8d180146531f1358f1
+// posts
+const searchClient = algoliasearch(
+  "KFWXVJ2EGD",
+  "181bd9ae00288f8d180146531f1358f1"
+);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <InstantSearch searchClient={searchClient} indexName="products">
+      <div className="app">
+        <div className="search-container">
+          <SearchBox />
+          <Hits hitComponent={Product} />
+          <Stats />
+          <Pagination />
+        </div>
+      </div>
+    </InstantSearch>
+  );
+}
+
+function Product({ hit }) {
+  return (
+    <a
+      className="product"
+      href={hit.url}
+      traget="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src={hit.image} alt={hit.name} />
+      <div>
+        <h3>{hit.brand}</h3>
+        <h2>{hit.name}</h2>
+        <p>${hit.price}</p>
+      </div>
+    </a>
   );
 }
 
